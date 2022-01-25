7b160f1cb63186cdf4f051a79dc80b75de6f00bb
Andreas Foth <andreas.foth@uni-leipzig.de>
2022-01-25 15:14:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Readme
+
+This is my test of a first complete repository on github testing status badges, sphinx for documentation, github pages and testing workflow. 
---
+++ calc.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import xarray as xr
+import numpy as np
+
+def calculate_something(x):
+    """
+    A short description.
+
+    A bit longer description.
+
+    Args:
+        variable (type): description
+
+    Returns:
+        type: description
+
+    Raises:
+        Exception: description
+
+    """
+
+    y = x^^2
+    return y
+
+def some_string_output(x):
+    """
+    A short description test.
+
+    A bit longer description.
+
+    Args:
+        variable (type): description
+
+    Returns:
+        type: description
+
+    Raises:
+        Exception: description
+
+    """
+    str = str(x)+' peter'
+    return str
+
+print(calculate_something(2))
+
+print(some_string_output(2))
