3bb66801dd2e3fd3c49a57d8deb0323fdf4cf358
Andreas Foth <45787527+WillyWallace@users.noreply.github.com>
2022-01-25 21:46:28 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,6 @@ version = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.napoleon',
